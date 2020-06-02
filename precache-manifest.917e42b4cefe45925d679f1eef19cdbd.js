831aa2ab3f416c2e394b22757bac13e9b80849eb
ReRuol <970960818ReRuol@users.noreply.github.com>
2020-06-02 13:07:01 +0800

    initial commit

---
+++ precache-manifest.917e42b4cefe45925d679f1eef19cdbd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a676cecf7d4f121d09e0dd42e51fb2",
+    "url": "/drag/index.html"
+  },
+  {
+    "revision": "2d4fda0829427ae62eb8",
+    "url": "/drag/static/css/main.e2a18571.chunk.css"
+  },
+  {
+    "revision": "d120f2024ca4a8b8ab87",
+    "url": "/drag/static/js/2.3bfcea85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drag/static/js/2.3bfcea85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4fda0829427ae62eb8",
+    "url": "/drag/static/js/main.a0a45b0f.chunk.js"
+  },
+  {
+    "revision": "57235319e1242f65c5ef",
+    "url": "/drag/static/js/runtime-main.ad71a2cc.js"
+  },
+  {
+    "revision": "9e26a14c2ba7a33aa8bbad6f315f4501",
+    "url": "/drag/static/media/background.9e26a14c.jpg"
+  },
+  {
+    "revision": "8d4d8aaeca20ea7a3df0d36061316e65",
+    "url": "/drag/static/media/background4.8d4d8aae.jpg"
+  }
+]);
